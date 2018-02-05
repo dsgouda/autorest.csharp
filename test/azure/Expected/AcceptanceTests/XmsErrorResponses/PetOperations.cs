9694232936ac5747be0bbb54cd4c06a3d4a2aded
Deepak Shankargouda <deshank@microsoft.com>
2018-02-05 11:05:36 -0800

    All regen done

--- test/azure/Expected/AcceptanceTests/XmsErrorResponses/PetOperations.cs
+++ test/azure/Expected/AcceptanceTests/XmsErrorResponses/PetOperations.cs
@@ -187,9 +187,9 @@ namespace Fixtures.Azure.AcceptanceTestsXmsErrorResponses
             var _result = new AzureOperationResponse<Pet>();
             _result.Request = _httpRequest;
             _result.Response = _httpResponse;
-            if (_httpResponse.Headers.Contains("get-pet-request"))
+            if (_httpResponse.Headers.Contains("x-ms-request-id"))
             {
-                _result.RequestId = _httpResponse.Headers.GetValues("get-pet-request").FirstOrDefault();
+                _result.RequestId = _httpResponse.Headers.GetValues("x-ms-request-id").FirstOrDefault();
             }
             // Deserialize Response
             if ((int)_statusCode == 200)
@@ -351,9 +351,9 @@ namespace Fixtures.Azure.AcceptanceTestsXmsErrorResponses
                 catch(CloudException ex)
                 {
                     // set the request id to exception
-                    if (_httpResponse.Headers.Contains("get-pet-request"))
+                    if (_httpResponse.Headers.Contains("x-ms-request-id"))
                     {
-                        ex.RequestId = _httpResponse.Headers.GetValues("get-pet-request").FirstOrDefault();
+                        ex.RequestId = _httpResponse.Headers.GetValues("x-ms-request-id").FirstOrDefault();
                     }
                     throw ex;
                 }
